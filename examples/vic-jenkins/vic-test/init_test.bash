04d559ef68a84b433e8800e902ee7da32730b19e
mathurn <mathurn@vmware.com>
2017-08-02 10:33:20 -0700

    updated test.

--- examples/vic-jenkins/vic-test/init_test.bash
+++ examples/vic-jenkins/vic-test/init_test.bash
@@ -1,43 +1,31 @@
 #!/usr/bin/env bash
 
-#      LOG_TEMP_DIR: install-logs
-#      DRONE_SERVER:  ${DRONE_SERVER}
-#      GITHUB_AUTOMATION_API_KEY:  ${GITHUB_AUTOMATION_API_KEY}
-#      DRONE_TOKEN:  ${DRONE_TOKEN}
-#      GS_PROJECT_ID: ${GS_PROJECT_ID}
-#      GS_CLIENT_EMAIL: ${GS_CLIENT_EMAIL}
-#      GS_PRIVATE_KEY: ${GS_PRIVATE_KEY}
-
-export DRONE_BUILD_NUMBER="034083375"
-
-export TEST_BUILD_IMAGE=""
-export TEST_URL_ARRAY=(${ESX_HOST_0} ${ESX_HOST_1} ${ESX_HOST_2} ${ESX_HOST_3})
-export TEST_RESOURCE=/${DATACENTER_NAME}/host/${CLUSTER_NAME}
-export TEST_TIMEOUT=60s
-export VIC_ESX_TEST_DATASTORE=/${DATACENTER_NAME}/datastore/${DATASTORE_NAME}
-export VIC_ESX_TEST_URL=${ESX_USERNAME}:${ESX_PASSWORD}@${ESX_URL}
-
-export BIN=$GOPATH/src/github.com/vmware/vic/bin
-export GOPATH=$GOPATH
-export SHELL=/bin/bash
-
-export TEST_USERNAME=${ESX_USERNAME}
-export TEST_PASSWORD=${ESX_PASSWORD}
-export TEST_DATASTORE=${DATASTORE_NAME}
-export TEST_RESOURCE=${CLUSTER_NAME}
-export TEST_TIMEOUT=60s
-
-export GOVC_USERNAME=${ESX_USERNAME}
-export GOVC_PASSWORD=${ESX_PASSWORD}
-export GOVC_DATACENTER=${DATACENTER_NAME}
-export GOVC_DATASTORE=${DATASTORE_NAME}
-export GOVC_RESOURCE_POOL=${CLUSTER_NAME}
-export GOVC_HOST=${ESX_URL}
-export GOVC_INSECURE=true
-
-export BRIDGE_NETWORK=${BRIDGE_NETWORK}
-export PUBLIC_NETWORK=${PUBLIC_NETWORK}
-export DOMAIN=${ESX_TLS_CNAME}
-export REGISTRY_PASSWORD=${REGISTRY_PASSWORD}
-
-pybot --removekeywords TAG:secret --exclude skip tests/test-cases
+docker ${DOCKER_FLAGS} run                                                            \
+    -e TEST_BUILD_IMAGE=""                                                            \
+    -e TEST_URL_ARRAY="(${ESX_HOST_0} ${ESX_HOST_1} ${ESX_HOST_2} ${ESX_HOST_3})"     \
+    -e TEST_RESOURCE="/${DATACENTER_NAME}/host/${CLUSTER_NAME}"                       \
+    -e TEST_TIMEOUT=60s                                                               \
+    -e VIC_ESX_TEST_DATASTORE="/${DATACENTER_NAME}/datastore/${DATASTORE_NAME}"       \
+    -e VIC_ESX_TEST_URL="${ESX_USERNAME}:${ESX_PASSWORD}@${ESX_URL}"                  \
+    -e BIN=bin                                                                        \
+    -e GOPATH=/go                                                                     \
+    -e SHELL=/bin/bash                                                                \
+    -e TEST_USERNAME=${ESX_USERNAME}                                                  \
+    -e TEST_PASSWORD=${ESX_PASSWORD}                                                  \
+    -e TEST_DATASTORE=${DATASTORE_NAME}                                               \
+    -e TEST_RESOURCE=${CLUSTER_NAME}                                                  \
+    -e TEST_TIMEOUT=60s                                                               \
+    -e GOVC_USERNAME=${ESX_USERNAME}                                                  \
+    -e GOVC_PASSWORD=${ESX_PASSWORD}                                                  \
+    -e GOVC_DATACENTER=${DATACENTER_NAME}                                             \
+    -e GOVC_DATASTORE=${DATASTORE_NAME}                                               \
+    -e GOVC_RESOURCE_POOL=${CLUSTER_NAME}                                             \
+    -e GOVC_HOST=${ESX_URL}                                                           \
+    -e GOVC_INSECURE=true                                                             \
+    -e BRIDGE_NETWORK=${BRIDGE_NETWORK}                                               \
+    -e PUBLIC_NETWORK=${PUBLIC_NETWORK}                                               \
+    -e DOMAIN=${ESX_TLS_CNAME}                                                        \
+    -e DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER}                                       \
+    -v ${GOPATH}:/go                                                                  \
+    -w /go/src/github.com/vmware/vic                                                  \
+    gcr.io/eminent-nation-87317/vic-integration-test:1.33
